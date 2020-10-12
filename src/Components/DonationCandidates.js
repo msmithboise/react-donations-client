3716e2ec7646eab82b65b49846bbb40b183f31eb
Michael <msmithboise@gmail.com>
2020-10-11 20:20:55 -0600

    git init

---
+++ src/Components/DonationCandidates.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react"
+
+const DonationCandidates = (props) => {
+    return (<div>from DonationCandidates</div>);
+}
+ 
+export default DonationCandidates;
---
+++ src/Components/DonationCanditateForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DonationCandidatesForm = (props) => {
+    return ( <div>from DonationCandidatesForm</div> );
+}
+ 
+export default DonationCandidatesForm;
