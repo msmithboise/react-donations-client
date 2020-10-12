6ab5c51a033027d635a732629a4a793dc7288389
Michael <msmithboise@gmail.com>
2020-10-11 21:05:19 -0600

    added dev tool extension

--- src/Actions/Store.js
+++ src/Actions/Store.js
@@ -6,5 +6,7 @@ export const store = createStore(
     reducers,
     {},
 compose(
-    applyMiddleware(thunk))
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 )
