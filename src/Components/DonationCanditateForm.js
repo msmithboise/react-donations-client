1300159a7bab51147c5be1bc5bd9c47aaa6f5e85
Michael <msmithboise@gmail.com>
2020-10-11 22:40:13 -0600

    added form

--- src/Components/DonationCanditateForm.js
+++ src/Components/DonationCanditateForm.js
@@ -1,7 +1,71 @@
-import React from "react";
+import { TextField, Grid} from "@material-ui/core";
+import React, {useState} from "react";
+
+const initialFieldValues = {
+    fullName: '',
+    mobile: '',
+    email: '',
+    again: '',
+    bloodGroup: '',
+    address: ''
+}
 
 const DonationCandidatesForm = (props) => {
-    return ( <div>from DonationCandidatesForm</div> );
+    const {values,setValues} = useState(initialFieldValues)
+
+    const handleInputChange = e =>{
+        const{name, value}= e.target
+        setValues({
+            ...values,
+            [name]:value
+        })
+    }
+
+    return ( <form autoComplete="off" noValidate>
+
+        <Grid container>
+            <Grid item xs={6}>
+            <TextField
+            name="fullName"
+            variant="outlined"
+            label="Full Name"
+            value={values.fullName}
+            onChange={handleInputChange}
+            />
+             <TextField
+            name="email"
+            variant="outlined"
+            label="Email"
+            value={values.email}
+            onChange={handleInputChange}
+            />
+             <div>bloodGroup</div>
+            </Grid>
+            <Grid item xs={6}>
+            <TextField
+            name="mobile"
+            variant="outlined"
+            label="Mobile"
+            value={values.mobile}
+            onChange={handleInputChange}
+            /> <TextField
+            name="age"
+            variant="outlined"
+            label="Age"
+            value={values.age}
+            onChange={handleInputChange}
+            /> <TextField
+            name="address"
+            variant="outlined"
+            label="Address"
+            value={values.address}
+            onChange={handleInputChange}
+            />
+            </Grid>
+        </Grid>
+
+
+    </form> );
 }
  
 export default DonationCandidatesForm;
